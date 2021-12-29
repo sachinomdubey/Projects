f175674891be376cfd5804543169ee2dc3d9f03c
sachinomdubey <70533204+sachinomdubey@users.noreply.github.com>
2021-12-29 12:57:28 +0530

    Update navsuccess_edit.ino

--- Autonomous Navigation/UGV/ESP32/IDE/Beacon_tracking/navsuccess_edit.ino
+++ Autonomous Navigation/UGV/ESP32/IDE/Beacon_tracking/navsuccess_edit.ino
@@ -1,107 +1,85 @@
 #include <WiFi.h>
 #include "analogWrite.h"
 
-void Stop();
-void rightTurn();
-void leftTurn();
-int maxof(int a,int b,int c);
-int check_RSSI();
-void Back();
-void Right();
-void Left();
-void Forward();
-
+//Setting up WiFi server and Client
 String  ClientRequest;
 WiFiServer server(80);
-
 WiFiClient client;
-
 String myresultat;
-
 String ReadIncomingRequest(){
-while(client.available()) {
-ClientRequest = (client.readStringUntil('\r'));
- if ((ClientRequest.indexOf("HTTP/1.1")>0)&&(ClientRequest.indexOf("/favicon.ico")<0)){
-myresultat = ClientRequest;
-}
-}
-return myresultat;
+  while(client.available()) {
+    ClientRequest = (client.readStringUntil('\r'));
+    if ((ClientRequest.indexOf("HTTP/1.1")>0)&&(ClientRequest.indexOf("/favicon.ico")<0)){
+      myresultat = ClientRequest;
+    }
+  }
+  return myresultat;
 }
 
+//Defining pins for motor1 and motor2 direction control
 const int Motor_L_F = 16;
 const int Motor_L_B = 17;
 const int Motor_R_F = 18;
 const int Motor_R_B = 19;
+
+//Connecting to the beacon in order to read RSSI values
 void setup()
 {
-ClientRequest = "";
-Serial.begin(115200);
+  ClientRequest = "";
+  Serial.begin(115200);
   WiFi.disconnect();
   delay(3000);
   Serial.println("START");
-  WiFi.begin("Anil","anil1234");
-  while ((!(WiFi.status() == WL_CONNECTED))){
+  WiFi.begin("Sachin","sachin1234"); //Change this details as per your android phone
+  while ((!(WiFi.status() == WL_CONNECTED))) {
     delay(300);
     Serial.print("..");
-
   }
   Serial.println("Connected");
   Serial.println("Your IP is");
   Serial.println((WiFi.localIP()));
   server.begin();
-   pinMode(14, OUTPUT);
-    pinMode(15, OUTPUT);
-  pinMode(Motor_L_F, OUTPUT);
+  
+  pinMode(14, OUTPUT);  //Speed control output pins
+  pinMode(15, OUTPUT);
+  
+  pinMode(Motor_L_F, OUTPUT); //Directional control output pins
   pinMode(Motor_L_B, OUTPUT);
   pinMode(Motor_R_F, OUTPUT);
   pinMode(Motor_R_B, OUTPUT);
- 
 }
 
 
 void loop()
 {
-  analogWrite(14,170);
-  analogWrite(15,170);
-  int Frssi = check_RSSI();
-  while (Frssi>114)
+  analogWrite(14,170);                  //Speed control of motor-1
+  analogWrite(15,170);                  //Speed control of motor-2
+  int Frssi = check_RSSI();             //Read the RSSI value in "Forward" direction
+  while (Frssi>114)                     //Setting threshold for stopping the UGV on reaching close to beacon
   {
     int Frssi = check_RSSI();
   }
-  rightTurn();
-  int Rrssi = check_RSSI();
-  leftTurn();
-  leftTurn();
-  int Lrssi = check_RSSI();
-  int mxOf=maxof(Frssi,Rrssi,Lrssi);
-  Serial.print("max = ");
-  Serial.println(mxOf);
+  rightTurn();                          // Turn Right in-place 
+  int Rrssi = check_RSSI();             // Read the RSSI value in "Right" direction
+  leftTurn();                           //Turn Left in-Place to get back to "Forward" direction
+  leftTurn();                           //Turn left in-place to turn to "Left" direction
+  int Lrssi = check_RSSI();             //Read the RSSI value in "Left" direction
+  int mxOf=maxof(Frssi,Rrssi,Lrssi);    //Find the max value amoung the RSSI values read in the three directions
+  //Serial.print("max = ");
+  //Serial.println(mxOf);
+
+  //Turn towards the direction of maximum RSSI and move towards that direction
   for (int i=0;i<mxOf;i++)
   {
     rightTurn();
-    
   }
-  Back();
+  Forward();
   delay(700);
   Stop();
   delay(300);
 }
-void rightTurn()
-{
-  Right();
-  delay(300);
-  Stop();
-  delay(400);
-}
 
-void leftTurn()
-{
-  Left();
-  delay(300);
-  Stop();
-  delay(400);
-}
-int maxof(int a,int b,int c)
+int maxof(int a,int b,int c)  //Function to find max amoung the three RSSI values
 {
   int mx = 1;
   if (b>a)
@@ -119,7 +97,7 @@ int maxof(int a,int b,int c)
  return mx;
 }
 
-int check_RSSI(){
+int check_RSSI(){               // Function to read RSSI values from the android phone (beacon)
   int rssi=0;
   int x;
   for(int j=0;j<20;j++)
@@ -127,64 +105,58 @@ int check_RSSI(){
    x=WiFi.RSSI();
   rssi = rssi + x;
   }
-  rssi = rssi / 40;
+  rssi = rssi / 40;         //Averaging over 20 points for stable readings
   Serial2.write(rssi);
   Serial.println(rssi);
   return rssi;
 }
 
+void rightTurn(){               //Turn right 90 degree in-place
+  Right();
+  delay(300);
+  Stop();
+  delay(400);
+}
 
-void Back(){
-  // Run Left Motor In Forward Direction
+void leftTurn(){                //Turn left 90 degree in-place
+  Left();
+  delay(300);
+  Stop();
+  delay(400);
+}
+
+void Forward(){                   //Move forward
   digitalWrite(Motor_L_F, HIGH);
   digitalWrite(Motor_L_B, LOW);
-
-  //Run Right Motor in Forward Direction
   digitalWrite(Motor_R_F, HIGH);
   digitalWrite(Motor_R_B, LOW);
   
   }
 
-void Right(){
-  //Stop Right Motor
+void Right(){                     //Move Right
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, LOW);
-
-  //Run Left Motor in Forward Direction
   digitalWrite(Motor_L_F, HIGH);
   digitalWrite(Motor_L_B, LOW);
-  
-  }
+}
 
-void Left(){
-  //Stop Left Motor
+void Left(){                      //Move Left
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, LOW);
-
-  //Run Right Motor in Forward Direction
   digitalWrite(Motor_R_F, HIGH);
   digitalWrite(Motor_R_B, LOW);
-  
-  }
+}
 
-void Forward(){
-  // Run Left Motor In Forward Direction
+void Back(){                      //Move Back
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, HIGH);
-
-  //Run Right Motor in Forward Direction
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, HIGH);
-  
-  }
+}
 
-void Stop(){
-  // Run Left Motor In Forward Direction
+void Stop(){                      //Stop
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, LOW);
-
-  //Run Right Motor in Forward Direction
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, LOW);
-  
-  }
+}
