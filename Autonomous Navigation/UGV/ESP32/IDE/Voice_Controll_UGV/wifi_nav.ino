c6d1078eb163546f376532d0b2d41799b2b350c8
sachinomdubey <70533204+sachinomdubey@users.noreply.github.com>
2021-12-29 07:41:14 +0530

    Update wifi_nav.ino

--- Autonomous Navigation/UGV/ESP32/IDE/Voice_Controll_UGV/wifi_nav.ino
+++ Autonomous Navigation/UGV/ESP32/IDE/Voice_Controll_UGV/wifi_nav.ino
@@ -2,139 +2,109 @@
 #include <WiFiClient.h> 
 #include <WebServer.h>
 
+//Declaring pins for motor1 and motor2 direction control
 const int Motor_L_F = 16;
 const int Motor_L_B = 17;
 const int Motor_R_F = 18;
 const int Motor_R_B = 19;
 
-WebServer server(80);
+WebServer server(80); //Initializing ESP32 as a web server
 
 void setup() {
   Serial.begin(115200);
+  
+  //Defining the motor directional control pins as output pins
   pinMode(Motor_L_F, OUTPUT);
   pinMode(Motor_L_B, OUTPUT);
   pinMode(Motor_R_F, OUTPUT);
   pinMode(Motor_R_B, OUTPUT);
   
   Serial.println("START");
-  WiFi.begin("Anil","anil1234");
-// Connecting WiFi
- while ((!(WiFi.status() == WL_CONNECTED))){
+  WiFi.begin("Sachin","sachin1234");  //Note: This line should be modified to the wifi hotspot SSID/Password of the android phone in use.
+  
+  // Connecting WiFi
+  while ((!(WiFi.status() == WL_CONNECTED))) {
     delay(300);
     Serial.print("..");
-
   }
-
-  //WiFi.mode(WIFI_AP);
-  //WiFi.softAP(ssid);
-// Starting WEB-server
- Serial.println("Connected");
+  // Starting WEB-server
+  Serial.println("Connected");
   Serial.println("Your IP is");
   Serial.println((WiFi.localIP()));
-    
-     server.on ( "/", HTTP_handleRoot );
-     server.onNotFound ( HTTP_handleRoot );
-     server.begin();    
+
+  server.on ( "/", HTTP_handleRoot );
+  server.onNotFound ( HTTP_handleRoot );
+  server.begin();    
      
 }
 
 void loop() {
   server.handleClient();
-   delay(50);
+  delay(50);
 }
 
 void HTTP_handleRoot(void) {
-String ani;
-//if( server.hasArg("State") ){
-       ani=server.arg("State");
-     //  Serial.print(ani);
- // }
-//  server.send ( 200, "text/html", "" );
-if (ani == "forward")
- {
-  Forward();
-  Serial.print("Forward");
- }
- else
- {
-  if (ani == "back")
+  String ani;
+  ani=server.arg("State");
+  if (ani == "forward")
   {
-     Serial.print("back");
-     Back();
+    Serial.print("Forward");
+    Forward();
   }
-  else
+  else if (ani == "back")
   {
-    if (ani== "right")
-    {
-       Serial.print("right");
-      Right();
-    }
-    else
-    {
-      if (ani=="left")
-      {
-         Serial.print("left");
-         Left();
-      }
-      else {
-         Serial.print("stop");
-         Stop();
-      }
-      }
-    }
+    Serial.print("back");
+    Back();
   }
- }
- void Forward(){
+  else if (ani== "right")
+  {
+    Serial.print("right");
+    Right();
+  }
+  else if (ani=="left")
+  {
+    Serial.print("left");
+    Left();
+  }
+  else 
+  {
+    Serial.print("stop");
+    Stop();
+  }
+}
  
+void Forward() { //Move Forward command
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, HIGH);
-
- 
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, HIGH);
-  
-  }
+}
 
-void Right(){
- 
+void Right() {  //Move Right command
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, HIGH);
-  
   digitalWrite(Motor_R_F, HIGH);
   digitalWrite(Motor_R_B, LOW);
-  
-  }
+}
 
-void Left(){
-  
+void Left() { //Move Left command
   digitalWrite(Motor_L_F, HIGH);
   digitalWrite(Motor_L_B, LOW);
-
-  
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, HIGH);
-  
-  }
+}
 
-void Back(){
- 
+void Back() { //Move Back command
   digitalWrite(Motor_L_F, HIGH);
   digitalWrite(Motor_L_B, LOW);
-
-  
   digitalWrite(Motor_R_F, HIGH);
-  digitalWrite(Motor_R_B, LOW);
-  
-  }
+  digitalWrite(Motor_R_B, LOW); 
+}
 
-void Stop(){
-  // Run Left Motor In Forward Direction
+void Stop() { //Move Stop command
   digitalWrite(Motor_L_F, LOW);
   digitalWrite(Motor_L_B, LOW);
-
- 
   digitalWrite(Motor_R_F, LOW);
   digitalWrite(Motor_R_B, LOW);
-  
-  }
+}
   
